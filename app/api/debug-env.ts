81da1b6e3d2c1870658186cc364b6f21fc28f2ec
gcjunior <gersonczjunior@gmail.com>
2026-03-26 16:11:47 -0600

    added debug env

--- app/api/debug-env.ts
+++ app/api/debug-env.ts
@@ -1,8 +1,8 @@
-// /api/debug-env
-export default function handler(req, res) {
-    res.json({
-      hasApiKey: !!process.env.RESEND_API_KEY,
-      email: process.env.CONTACT_TO_EMAIL || null,
-    });
-  }
-  
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    hasApiKey: !!process.env.RESEND_API_KEY,
+    email: process.env.CONTACT_TO_EMAIL || null,
+  });
+}
